f2d6853d7d174719cd804531cbddb24fe822878d
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-06-05 12:40:52 +0900

    Update item.js

--- kubejs/startup_scripts/item.js
+++ kubejs/startup_scripts/item.js
@@ -1,4 +1,6 @@
 onEvent('item.registry', event => {
   // item
-  event.create('incomplete_spore_blossom','create:sequenced_assembly').displayName(name).texture(customTexturePath)
+  event.create('incomplete_light_bulb','create:sequenced_assembly')
+    .texture('kubejs:item/immersiveengineering/incomplete_light_bulb')
+    .displayName('incomplete light bulb')
 })
