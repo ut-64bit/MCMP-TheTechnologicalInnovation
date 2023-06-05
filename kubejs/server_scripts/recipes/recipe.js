ae077c80e1e6160ca3b541b311fe5b82888f3158
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-06-05 11:11:01 +0900

    Update recipe.js

--- kubejs/server_scripts/recipes/recipe.js
+++ kubejs/server_scripts/recipes/recipe.js
@@ -1,5 +1,14 @@
-onEvent('recipes', e => {
-  // recipe
-  e.remove({output:'botania:flight_tiara'})
+onEvent('recipes', event => {
+  // botania
+  event.remove({output:'botania:flight_tiara'})
   
+  // tconstruct
+  event.remove({output:'tconstruct:earth_slime_sling'})
+  event.remove({output:'tconstruct:ender_slime_sling'})
+  event.remove({output:'tconstruct:ichor_slime_sling'})
+  event.remove({output:'tconstruct:sky_slime_sling'})
+  
+  event.remove({id:'tconstruct:smeltery/seared/grout'})
+  event.remove({id:'tconstruct:smeltery/seared/grout_multiple'})
+  event.recipes.create.mixing('2x tconstruct:grout', ['minecraft:clay_ball', '#minecraft:sand', 'minecraft:gravel'])
 })
