3061ad6133a2c6dc34de6a83a9af565a78695741
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-06-05 13:41:15 +0900

    update

--- kubejs/server_scripts/common.js
+++ kubejs/server_scripts/common.js
@@ -1,17 +1,22 @@
 onEvent('recipes', event => {
+  // all
+
   // botania
-  event.remove({output:'botania:flight_tiara'})
-  
+  event.remove({ output: 'botania:flight_tiara' })
+
   // tconstruct
-  event.remove({output:'tconstruct:earth_slime_sling'})
-  event.remove({output:'tconstruct:ender_slime_sling'})
-  event.remove({output:'tconstruct:ichor_slime_sling'})
-  event.remove({output:'tconstruct:sky_slime_sling'})
-  
-  event.remove({id:'tconstruct:smeltery/seared/grout'})
-  event.remove({id:'tconstruct:smeltery/seared/grout_multiple'})
+  event.remove({ output: 'tconstruct:earth_slime_sling' })
+  event.remove({ output: 'tconstruct:ender_slime_sling' })
+  event.remove({ output: 'tconstruct:ichor_slime_sling' })
+  event.remove({ output: 'tconstruct:sky_slime_sling' })
+
+  event.remove({ id: 'tconstruct:smeltery/seared/grout' })
+  event.remove({ id: 'tconstruct:smeltery/seared/grout_multiple' })
   event.recipes.create.mixing('2x tconstruct:grout', ['minecraft:clay_ball', '#minecraft:sand', 'minecraft:gravel'])
-  
+
   // ars_nouveau
-  event.remove({id:'ars_nouveau:warp_scroll'})
+  event.remove({ output: 'ars_nouveau:warp_scroll' })
+
+  // immersiveengineering
+  event.remove({output:'immersiveengineering:'})
 })
