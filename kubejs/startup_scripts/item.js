d815d2f4c4c66c8fbc7c7d7f68a8a8e949eda0e3
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-06-05 12:53:00 +0900

    Update item.js

--- kubejs/startup_scripts/item.js
+++ kubejs/startup_scripts/item.js
@@ -2,5 +2,5 @@ onEvent('item.registry', event => {
   // item
   event.create('incomplete_light_bulb','create:sequenced_assembly')
     .texture('kubejs:item/immersiveengineering/incomplete_light_bulb')
-    .displayName('incomplete light bulb')
+    .translationKey('item.kubejs.incomplete_light_bulb')
 })
