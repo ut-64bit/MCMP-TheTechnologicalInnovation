61f1cb8a3cd826f84c6bf7abde3cbb8a7b0736d5
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-06-05 12:14:28 +0900

    Update item.js

--- kubejs/startup_scripts/item.js
+++ kubejs/startup_scripts/item.js
@@ -1,3 +1,4 @@
-onEvent('item.registry', e => {
+onEvent('item.registry', event => {
   // item
+  event.create('incomplete_spore_blossom','create:sequenced_assembly').displayName(name).texture(customTexturePath)
 })
