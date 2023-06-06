b80e96d051d84a7f5d718b8c60ae181c7ef3c2d7
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-06-06 12:46:56 +0900

    updata

--- kubejs/client_scripts/client_script.js
+++ kubejs/client_scripts/client_script.js
@@ -1,1 +1,15 @@
+onEvent('jei.hide.items', event => {
+    // botanis
+    event.hide('botania:flight_tiara')
+    event.hide('botania:mana_fluxfield')
+    event.hide('botania:conjuration_catalyst')
 
+    // tconstruct
+    event.hide('tconstruct:earth_slime_sling')
+    event.hide('tconstruct:ender_slime_sling')
+    event.hide('tconstruct:ichor_slime_sling')
+    event.hide('tconstruct:sky_slime_sling')
+
+    // ars_nouveau
+    event.hide('ars_nouveau:warp_scroll')
+})
--- kubejs/startup_scripts/item.js
+++
@@ -1,6 +0,0 @@
-onEvent('item.registry', event => {
-  // item
-  event.create('incomplete_light_bulb', 'create:sequenced_assembly')
-    .texture('kubejs:item/immersiveengineering/incomplete_light_bulb')
-    .translationKey('item.kubejs.incomplete_light_bulb')
-})
